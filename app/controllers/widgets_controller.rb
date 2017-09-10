320fa57ed638a288def8a1a9b2488e82977f8e75
Thanos Bellos <contact@thanosbellos.com>
2017-09-10 10:58:06 +0300

    Add application layout

---
+++ app/controllers/widgets_controller.rb
@@ -0,0 +1,21 @@
+class WidgetsController < ApplicationController
+  def index
+  end
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
